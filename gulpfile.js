5bc0f2b2dda2be1cd8c06673d171fed2e64c1d12
Keven Jesus <falecom@keven.com.br>
2017-02-13 14:35:52 -0200

    tasks criada corretamente para compilar o sass em css e minificar o html

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,51 @@
-var gulp = require("gulp");
-var del = require("del");
-gulp.task('limpa',function(){
-	del("dist/css")
-})
-gulp.task('move-css',function(){
-	return gulp.src('./source/scss/style.scss')
-			.pipe(gulp.dest('./dist/css'));
-})
-gulp.task('layout-css',function(){
-	return gulp.src('./source/scss/layout.scss')
-			.pipe(gulp.dest('./dist/css'));
-})
-gulp.task('base-css',function(){
-	return gulp.src('./source/scss/base.scss')
-			.pipe(gulp.dest('./dist/css'));
-})
-gulp.task('move-js',function(){
-	return gulp.src('./source/js/style.js')
-			.pipe(gulp.dest('./dist/js'));
-})
+var gulp 		= require("gulp");
+var sass 		= require("gulp-sass");
+var htmlmin = require("gulp-htmlmin");
+var del 		= require("del");
 
-gulp.task('view-css',function(){
-	gulp.watch('./source/scss/style.scss',['move-css']);
-	gulp.watch('./source/scss/layot.scss',['layout-css']);
-	gulp.watch('./source/scss/base.scss',['base-css']);
+/*
+  Apaga os arquivos css
+*/
+
+gulp.task("clean:css", function(){
+	return del("dist/css");
+});
+
+/*
+  Apaga o arquivo html
+*/
+
+gulp.task("clean:html", function(){
+	return del("dist/*.html");
+});
+
+
+/*
+  transforma o sass em css
+*/
+
+gulp.task("sass", ['clean:css'], function(){
+	return gulp.src("./source/scss/*.scss")
+				.pipe(sass({outputStyle: "compressed"}))
+				.pipe(gulp.dest("dist/css"));
+});
+
+
+/*
+  minifica o html
+*/
+
+gulp.task("html", ['clean:html'], function(){
+	return gulp.src("./source/*.html")
+				.pipe(htmlmin({collapseWhitespace:true}))
+				.pipe(gulp.dest("dist"))
+});
+
+/*
+  escuta os arquivos sass e html
+*/
+
+gulp.task("watch", function(){
+	gulp.watch("./source/**/*.scss", ['sass']);
+	gulp.watch("./source/*.html", ['html']);
 });
